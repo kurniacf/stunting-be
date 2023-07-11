685540503a1ebb867d4b5d2ee749def6f1550d62
christhopermarcelino <christhopermarcelino@gmail.com>
2023-07-11 21:03:05 +0700

    feat: pass email value from token

--- pkg/middleware/jwt_auth_middleware.go
+++ pkg/middleware/jwt_auth_middleware.go
@@ -1,28 +1,43 @@
 package middleware
 
 import (
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
+		bearerToken := c.GetHeader("Authorization")
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		isTokenValid := strings.HasPrefix(bearerToken, "Bearer ")
+		if !isTokenValid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		}
+
+		tokenString := strings.Split(bearerToken, " ")[1]
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		}
+
+		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if jwt.GetSigningMethod("HS256") != token.Method {
 				return nil, jwt.ErrSignatureInvalid
 			}
 			return []byte(os.Getenv("JWT_SECRET")), nil
 		})
 
-		if err != nil || !token.Valid {
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
+
+		c.Set("email", claims["email"])
 
 		c.Next()
 	}
